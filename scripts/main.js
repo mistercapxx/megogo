d5e786d54dc951b9d6207442311a24bfcbfee1b9
Vladimir <test@example.com>
2024-05-31 12:18:24 +0300

    Megogo yo

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    new fullpage('#fullpage', {
+        autoScrolling: true,
+        scrollHorizontally: true,
+        responsiveWidth: 768,
+    });
+});
